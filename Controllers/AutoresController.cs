62deed59ba4cf9400ae165d59b7550e81754ce65
RicardoTenorio <Tenricardo2501@gmail.com>
2023-08-18 22:31:17 -0600

    Primer Commit

---
+++ Controllers/AutoresController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPIAutores.Entidades;
+
+namespace WebAPIAutores.Controllers
+{
+    [ApiController]
+    [Route("api/autores")]
+    public class AutoresController:ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Autor>> Get()
+        {
+            return Ok(new List<Autor>()
+            {
+                new Autor()
+                {
+                    Id = 1,
+                    Nombre="Claudia"
+                },
+                new Autor()
+                {
+                    Id=2,
+                    Nombre="Felipe"
+                }
+            });
+        }
+        
+    }
+}
